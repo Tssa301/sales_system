c0a5f485c7aab4d0fafb6219a72444bab8abf638
Tiago Ssa <tiago.silva301@gmail.com>
2022-04-29 06:45:17 +0100

    Integrating Filter with the sales table

--- front-web/src/components/sales-table/index.tsx
+++ front-web/src/components/sales-table/index.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useMemo, useState } from 'react';
+import { FilterData, Sales, SalesResponse } from '../../types';
+import { formatDate, formatGender, formatPrice } from '../../utils/formatters';
+import { buildFilterParams, makeRequest } from '../../utils/request';
 import './styles.css';
 
-function SalesTable() {
+type Props = {
+  filterData?: FilterData;
+};
+
+const extraParams = {
+  page: 0,
+  size: 12,
+  sort: 'date,desc'
+};
+
+function SalesTable({ filterData }: Props) {
+  const [sales, setSales] = useState<Sales[]>([]);
+  const params = useMemo(() => buildFilterParams(filterData, extraParams), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesResponse>('/sales', { params })
+      .then((response) => {
+        setSales(response.data.content);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales');
+      });
+  }, [params]);
+
   return (
     <div className="sales-table-container base-card">
       <h3 className="sales-table-title">Sales recently</h3>
@@ -17,42 +45,17 @@ function SalesTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>#341</td>
-            <td>01/04/2022</td>
-            <td>Male</td>
-            <td>Cloths and accessories</td>
-            <td>USA</td>
-            <td>Credit</td>
-            <td>$ 45,000.00</td>
-          </tr>
-          <tr>
-            <td>#341</td>
-            <td>01/04/2022</td>
-            <td>Male</td>
-            <td>Cloths and accessories</td>
-            <td>USA</td>
-            <td>Credit</td>
-            <td>$ 45,000.00</td>
-          </tr>
-          <tr>
-            <td>#341</td>
-            <td>01/04/2022</td>
-            <td>Male</td>
-            <td>Cloths and accessories</td>
-            <td>USA</td>
-            <td>Credit</td>
-            <td>$ 45,000.00</td>
-          </tr>
-          <tr>
-            <td>#341</td>
-            <td>01/04/2022</td>
-            <td>Male</td>
-            <td>Cloths and accessories</td>
-            <td>USA</td>
-            <td>Credit</td>
-            <td>$ 45,000.00</td>
-          </tr>
+          {sales.map((sale) => (
+            <tr key={sale.id}>
+              <td>#{sale.id}</td>
+              <td>{formatDate(sale.date)}</td>
+              <td>{formatGender(sale.gender)}</td>
+              <td>{sale.categoryName}</td>
+              <td>{sale.storeName}</td>
+              <td>{sale.paymentMethod}</td>
+              <td>{formatPrice(sale.total)}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
